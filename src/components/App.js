97a7bbd4da654aa3b1b5618cfee3e2af9a28d916
react.solvit <react.solvit@gmail.com>
2022-06-29 10:35:58 +0200

    whatsapp main section layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>App</div>
+   <>
+   <aside className="messages-list"></aside>
+   <section className="messages-view"></section>
+   </>
   );
 }
 
